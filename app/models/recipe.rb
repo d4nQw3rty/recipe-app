6a1c7cfef2403910f2cf546cc9159fa70a5c4113
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-02-06 18:16:28 -0300

    remove unneeded validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,4 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, foreign_key: 'recipe_id'
-  validates :preparation_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
