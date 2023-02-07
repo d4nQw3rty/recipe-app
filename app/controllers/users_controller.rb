15ad6bafabfd9604e0a0bb0ae47a4656107e8212
d4nQw3rty <danlinmoy@gmail.com>
2023-02-07 16:52:26 -0400

    Delete create from users controller and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,6 @@ class UsersController < ApplicationController
     @recipes = @user.recipes
   end
 
-  # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render :show, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
   def user_params
     params.require(:user).permit(:name)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "users#index"
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show ]
 end
