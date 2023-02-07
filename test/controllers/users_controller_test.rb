5bae004937349d4fe2cfff354db1edd721d091af
d4nQw3rty <danlinmoy@gmail.com>
2023-02-07 12:54:38 -0400

    Create users_controller_test.rb

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should get index' do
+    get users_url
+    assert_response :success
+  end
+
+  test 'should show user' do
+    get user_url(@user)
+    assert_response :success
+  end
+end
