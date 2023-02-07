7a3b06fcfbd4509c790f1e7cba4463fedb7b4d60
d4nQw3rty <danlinmoy@gmail.com>
2023-02-07 16:47:50 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render :show, status: :created, location: @user
+      redirect_to @user, notice: 'User was successfully created.'
     else
-      render json: @user.errors, status: :unprocessable_entity
+      redirect_to users_path, notice: 'User was not created.'
     end
   end
 
