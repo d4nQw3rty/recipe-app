6a9e4b5a3e57e8d3f93083d4f92ce6ecd5b04ad2
d4nQw3rty <danlinmoy@gmail.com>
2023-02-07 16:47:35 -0400

    Create foods_controller.rb

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,38 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def create
+    @food = Food.new(food_params)
+
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
+    else
+      redirect_to @food, notice: 'Food was not created.'
+    end
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to @food, notice: 'Food was successfully updated.'
+    else
+      redirect_to @food, notice: 'Food was not updated.'
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+
+    redirect_to foods_path, notice: 'Food was successfully destroyed.'
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
+end
