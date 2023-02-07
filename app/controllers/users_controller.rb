ec41299946c0e5848e9d50355ce11345b401efe3
d4nQw3rty <danlinmoy@gmail.com>
2023-02-07 12:53:43 -0400

    Create users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1 or /users/1.json
+  def show
+    @user = User.find(params[:id])
+    @recipes = @user.recipes
+  end
+
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render :show, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
