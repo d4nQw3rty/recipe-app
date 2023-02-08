ba7ac5b4d821e8c49efa67bd9dc8042c76a2e237
d4nQw3rty <danlinmoy@gmail.com>
2023-02-08 16:10:41 -0400

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   # GET /users/1 or /users/1.json
   def show
     @user = User.find(params[:id])
-    @current_user = current_user
     @recipes = @user.recipes
   end
 
