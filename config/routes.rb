9fd3a7374375e287eb0daa669f1d6742565ff3d6
d4nQw3rty <danlinmoy@gmail.com>
2023-02-08 08:36:18 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "users#index"
-  resources :users, only: [:index, :show, :create]
-  resources :foods
+  resources :users, only: [:index, :show]
+  resources :foods, only: [:index, :create, :new,  :destroy]
 end
