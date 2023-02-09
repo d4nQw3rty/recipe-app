d3ba0db1942552ec26870a081937c221aa9459b7
d4nQw3rty <danlinmoy@gmail.com>
2023-02-09 09:00:44 -0400

    Add shopping_list to recipe_foods controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,6 +4,16 @@ class RecipeFoodsController < ApplicationController
     @foods = Food.where(user_id: current_user.id)
   end
 
+  def shopping_list
+    @recipefoods = RecipeFood.includes(:food).where(recipe_id: params[:id])
+    @recipe = Recipe.find(params[:id])
+    @recipefoods.each do |recipefood|
+      @food = Food.find(recipefood.food_id)
+      @food.quantity = @food.quantity - recipefood.quantity
+      @food.save
+    end
+  end
+
   def create
     @recipefood = RecipeFood.new(params.permit(:quantity, :food_id, :recipe_id))
     if @recipefood.save
