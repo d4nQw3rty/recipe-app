f095484fd200ecf42b4e6e67e29cc11de61d3fa2
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-02-10 13:44:51 -0300

    Add after:all cleanup to prevent interference

--- spec/integration/recipes_index_spec.rb
+++ spec/integration/recipes_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'Recipes Public', type: :system do
-  describe 'public' do
+RSpec.describe 'Recipes Index', type: :system do
+  describe 'recipes' do
     before :all do
       @user = User.new(name: 'nameofuser', email: 'asdt560@gmail.com', password: '6letters',
                        encrypted_password: '$2a$12$192AtELpNZ0aZCfnSxs35umQYmbSn52FK8ML/vY.iZvDW4FvkvHn2')
@@ -11,25 +11,19 @@ RSpec.describe 'Recipes Public', type: :system do
       sign_in @user
       @food1 = Food.create(name: 'foodstuff', measurement_unit: 'gr', price: 20, quantity: 1, user_id: @user.id)
       @recipe1 = Recipe.create(name: 'foodrecipe', preparation_time: '1.5', cooking_time: '1', description: 'lorem',
-        public: true, user_id: @user.id)
-      @recipe2 = Recipe.create(name: 'privatefoodrecipe', preparation_time: '1.5', cooking_time: '1', description: 'lorem',
-        public: false, user_id: @user.id)
+                               public: true, user_id: @user.id)
       @recipe1food = RecipeFood.create(quantity: 5, food_id: @food1.id, recipe_id: @recipe1.id)
     end
 
-    it 'displays the recipes' do
-      visit public_recipes_path
-      expect(page).to have_content('Recipes')
+    it 'displays recipes' do
+      visit recipes_path
       expect(page).to have_content('foodrecipe')
-      expect(page).to have_content("#{@recipe1.total_items}")
-      expect(page).to have_content("#{@recipe1.total_price}")
-      expect(page).to_not have_content('privatefoodrecipe')
+      expect(page).to have_content('lorem')
     end
 
     after :all do
       @recipe1food.destroy
       @recipe1.destroy
-      @recipe2.destroy
       @food1.destroy
       @user.destroy
     end
--- spec/integration/shopping_list_spec.rb
+++ spec/integration/shopping_list_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'Recipes Public', type: :system do
-  describe 'public' do
+RSpec.describe 'Shopping list', type: :system do
+  describe 'shopping' do
     before :all do
       @user = User.new(name: 'nameofuser', email: 'asdt560@gmail.com', password: '6letters',
                        encrypted_password: '$2a$12$192AtELpNZ0aZCfnSxs35umQYmbSn52FK8ML/vY.iZvDW4FvkvHn2')
@@ -11,25 +11,20 @@ RSpec.describe 'Recipes Public', type: :system do
       sign_in @user
       @food1 = Food.create(name: 'foodstuff', measurement_unit: 'gr', price: 20, quantity: 1, user_id: @user.id)
       @recipe1 = Recipe.create(name: 'foodrecipe', preparation_time: '1.5', cooking_time: '1', description: 'lorem',
-        public: true, user_id: @user.id)
-      @recipe2 = Recipe.create(name: 'privatefoodrecipe', preparation_time: '1.5', cooking_time: '1', description: 'lorem',
-        public: false, user_id: @user.id)
+                               public: true, user_id: @user.id)
       @recipe1food = RecipeFood.create(quantity: 5, food_id: @food1.id, recipe_id: @recipe1.id)
     end
 
-    it 'displays the recipes' do
-      visit public_recipes_path
-      expect(page).to have_content('Recipes')
-      expect(page).to have_content('foodrecipe')
-      expect(page).to have_content("#{@recipe1.total_items}")
-      expect(page).to have_content("#{@recipe1.total_price}")
-      expect(page).to_not have_content('privatefoodrecipe')
+    it 'displays shopping list' do
+      visit shopping_list_path(@recipe1.id)
+      expect(page).to have_content('Shopping list')
+      expect(page).to have_content('foodstuff')
+      expect(page).to have_content('4 gr')
+      expect(page).to have_content('80')
     end
-
     after :all do
       @recipe1food.destroy
       @recipe1.destroy
-      @recipe2.destroy
       @food1.destroy
       @user.destroy
     end
