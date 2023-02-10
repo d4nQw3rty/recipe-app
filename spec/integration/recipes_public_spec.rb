a3f05bf4aaf799bba0a65b2ca938083cf17a259d
d4nQw3rty <danlinmoy@gmail.com>
2023-02-10 13:09:06 -0400

    Update recipes_public_spec.rb

--- spec/integration/recipes_public_spec.rb
+++ spec/integration/recipes_public_spec.rb
@@ -1,37 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'Recipes Public', type: :system do
-  describe 'public' do
-    before :all do
+RSpec.describe 'Recipes', type: :system do
+  describe 'public' do     
+   before :all do
       @user = User.new(name: 'nameofuser', email: 'asdt560@gmail.com', password: '6letters',
                        encrypted_password: '$2a$12$192AtELpNZ0aZCfnSxs35umQYmbSn52FK8ML/vY.iZvDW4FvkvHn2')
       @user.skip_confirmation!
       @user.confirm
       @user.save
-      sign_in @user
-      @food1 = Food.create(name: 'foodstuff', measurement_unit: 'gr', price: 20, quantity: 1, user_id: @user.id)
-      @recipe1 = Recipe.create(name: 'foodrecipe', preparation_time: '1.5', cooking_time: '1', description: 'lorem',
-        public: true, user_id: @user.id)
-      @recipe2 = Recipe.create(name: 'privatefoodrecipe', preparation_time: '1.5', cooking_time: '1', description: 'lorem',
-        public: false, user_id: @user.id)
-      @recipe1food = RecipeFood.create(quantity: 5, food_id: @food1.id, recipe_id: @recipe1.id)
-    end
-
-    it 'displays the recipes' do
+      @food = Food.create(name: 'foodstuff', measurement_unit: 'gr', price: 20, quantity: 1, user_id: @user.id)
+      @recipe = Recipe.create(name: 'foodrecipe', preparation_time: '1.5', cooking_time: '1', description: 'lorem',
+                              public: true, user_id: @user.id)
+      @recipefood = RecipeFood.create(quantity: 5, food_id: @food.id, recipe_id: @recipe.id)
+      sign_in @user      
+   end
+    it 'displays the users name' do
       visit public_recipes_path
-      expect(page).to have_content('Recipes')
-      expect(page).to have_content('foodrecipe')
-      expect(page).to have_content("#{@recipe1.total_items}")
-      expect(page).to have_content("#{@recipe1.total_price}")
-      expect(page).to_not have_content('privatefoodrecipe')
+      expect(page).to have_content(@user.name) #1
+      expect(page).to have_content(@recipe.name) #2
+      expect(page).to have_content('Total price: $100') #3
+      expect(page).to have_content('Total food items: 1') #4
+      expect(page).to have_content('By: ' + @user.name) #5
     end
-
     after :all do
-      @recipe1food.destroy
-      @recipe1.destroy
-      @recipe2.destroy
-      @food1.destroy
       @user.destroy
+      @food.destroy
+      @recipe.destroy
+      @recipefood.destroy
     end
-  end
+  end  
 end
